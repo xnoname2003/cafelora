66ee181835bdf37a164d7b344de0970f98f7d1f1
Chaerul Cahyadi <chaeruladi15@gmail.com>
2025-12-22 19:44:19 +0700

    Add Transactions Policy

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -173,7 +173,8 @@ class TransactionResource extends Resource
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
-            ]);
+            ])
+            ->defaultSort('created_at', 'desc');
     }
 
     public static function getRelations(): array

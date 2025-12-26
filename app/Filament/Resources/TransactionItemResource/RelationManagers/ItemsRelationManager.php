ac8f96e547f9a12c16d469b21466fa60a0e9e82a
Chaerul Cahyadi <chaeruladi15@gmail.com>
2025-12-27 02:29:52 +0700

    Fix bug at TransactionsItemResource

--- app/Filament/Resources/TransactionItemResource/RelationManagers/ItemsRelationManager.php
+++ app/Filament/Resources/TransactionItemResource/RelationManagers/ItemsRelationManager.php
@@ -1,14 +1,12 @@
 <?php
 
-namespace App\Filament\Resources\TransactionResource\RelationManagers;
+namespace App\Filament\Resources\TransactionItemResource\RelationManagers;
 
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ItemsRelationManager extends RelationManager
 {
@@ -16,21 +14,24 @@ class ItemsRelationManager extends RelationManager
 
     public function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                Forms\Components\Select::make('menu_id')
+        return $form->schema([
+            Forms\Components\Select::make('menu_id')
                 ->relationship('menu', 'name')
                 ->required(),
+
             Forms\Components\Select::make('variant_id')
                 ->relationship('variant', 'name')
                 ->nullable(),
+
             Forms\Components\TextInput::make('quantity')
                 ->numeric()
                 ->default(1)
                 ->required(),
+
             Forms\Components\TextInput::make('price')
                 ->numeric()
                 ->required(),
+
             Forms\Components\TextInput::make('subtotal')
                 ->numeric()
                 ->required(),
@@ -42,24 +43,11 @@ class ItemsRelationManager extends RelationManager
         return $table
             ->recordTitleAttribute('menu.name')
             ->columns([
-                Tables\Columns\TextColumn::make('menu.name')
-                    ->label('Menu'),
-                Tables\Columns\TextColumn::make('variant.name')
-                    ->label('Varian'),
+                Tables\Columns\TextColumn::make('menu.name')->label('Menu'),
+                Tables\Columns\TextColumn::make('variant.name')->label('Varian'),
                 Tables\Columns\TextColumn::make('quantity'),
-                Tables\Columns\TextColumn::make('price')
-                    ->money('IDR'),
-                Tables\Columns\TextColumn::make('subtotal')
-                    ->money('IDR'),
-            ])
-            ->filters([
-                //
-            ])
-            ->headerActions([
-                //
-            ])
-            ->actions([
-                //
+                Tables\Columns\TextColumn::make('price')->money('IDR'),
+                Tables\Columns\TextColumn::make('subtotal')->money('IDR'),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([

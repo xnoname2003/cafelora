dddde28e45eab80f3ec94dbd3cf19dd87278c740
Chaerul Cahyadi <chaeruladi15@gmail.com>
2025-12-27 13:17:03 +0700

    Fix VerifyCSRFToken

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [
-            \App\Http\Middleware\VerifyCsrfToken::class,
+        $middleware->validateCsrfTokens(except: [
+            'midtrans/notify',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
